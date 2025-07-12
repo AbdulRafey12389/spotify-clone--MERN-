8c4f577bb33457ec2970e54f6188c14b162c949d
Abdul Rafey <abdulrafey12389@gmail.com>
2025-07-12 11:07:58 -0700

    deployment changes

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,21 @@
 // Importing Nodes modules
-import express from 'express';
-import dotenv from 'dotenv';
-import { clerkMiddleware } from '@clerk/express';
-import fileUpload from 'express-fileupload';
-import path from 'path';
-import cors from 'cors';
-import { initializeSocket } from './src/lib/socket.js';
-
+import express from "express";
+import dotenv from "dotenv";
+import { clerkMiddleware } from "@clerk/express";
+import fileUpload from "express-fileupload";
+import path from "path";
+import cors from "cors";
+import { initializeSocket } from "./src/lib/socket.js";
 
 // custom modules...
-import { connectDB, disConnectDB } from './src/lib/db.js';
-import userRoute from './src/routes/user.route.js';
-import adminRoute from './src/routes/admin.route.js';
-import authRoute from './src/routes/auth.route.js';
-import songRoute from './src/routes/song.route.js';
-import albumRoute from './src/routes/album.route.js';
-import statRoute from './src/routes/stat.route.js';
-import { createServer } from 'http';
-
+import { connectDB, disConnectDB } from "./src/lib/db.js";
+import userRoute from "./src/routes/user.route.js";
+import adminRoute from "./src/routes/admin.route.js";
+import authRoute from "./src/routes/auth.route.js";
+import songRoute from "./src/routes/song.route.js";
+import albumRoute from "./src/routes/album.route.js";
+import statRoute from "./src/routes/stat.route.js";
+import { createServer } from "http";
 
 // Initializing Express app
 const app = express();
@@ -30,28 +28,28 @@ const port = process.env.PORT;
 
 // Initializing socket.io server...
 const httpServer = createServer(app);
-initializeSocket(httpServer)
+initializeSocket(httpServer);
 
+// Parse JSON data from request body
+app.use(express.json());
 
-// Parse JSON data from request body  
-app.use(express.json()); 
-
-// Parse form data (application/x-www-form-urlencoded)  
-app.use(express.urlencoded({ extended: true })); 
+// Parse form data (application/x-www-form-urlencoded)
+app.use(express.urlencoded({ extended: true }));
 
 // create variable __dirname for path...
 const __dirname = path.resolve();
 
 // // Initializing file-upload package middleware for uploading files...
-app.use(fileUpload({
+app.use(
+  fileUpload({
     useTempFiles: true,
-    tempFileDir: path.join(__dirname, 'tmp'),
+    tempFileDir: path.join(__dirname, "tmp"),
     createParentPath: true,
     limits: {
-        fileSize: 10 * 1024 * 1024 // 10 MB MAX FILE SIZE...
-    }
-}));
-
+      fileSize: 10 * 1024 * 1024, // 10 MB MAX FILE SIZE...
+    },
+  })
+);
 
 // Initializing cors package middleware for cross-origin resource sharing...
 app.use(
@@ -63,11 +61,8 @@ app.use(
   })
 );
 
-
 // // protect our routes with clerk middleware...
 // app.use(clerkMiddleware());
-
-
 
 app.use(
   clerkMiddleware({
@@ -76,28 +71,33 @@ app.use(
   })
 );
 
-
-
-// Defining all routes and apis for pages... 
-app.use('/api/users', userRoute);
-app.use('/api/admin', adminRoute);
-app.use('/api/auth', authRoute);
-app.use('/api/songs', songRoute);
-app.use('/api/albums', albumRoute);
-app.use('/api/stats', statRoute);
-
+// Defining all routes and apis for pages...
+app.use("/", (req, res) => {
+  res.send("working");
+});
+app.use("/api/users", userRoute);
+app.use("/api/admin", adminRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/songs", songRoute);
+app.use("/api/albums", albumRoute);
+app.use("/api/stats", statRoute);
 
 // error handler route...
 app.use((err, req, res, next) => {
-    return res.status(500).json({message: process.env.NODE_ENV === 'production' ? 'Internal server error' : err.message});
+  return res
+    .status(500)
+    .json({
+      message:
+        process.env.NODE_ENV === "production"
+          ? "Internal server error"
+          : err.message,
+    });
 });
-
 
 // Starting the server and listening on the specified port
 const server = httpServer.listen(port, () => {
-    console.log(`App listening on  http://localhost:${port}!`);
-    connectDB();
+  console.log(`App listening on  http://localhost:${port}!`);
+  connectDB();
 });
 
-
-server.on('close', async () => await disConnectDB());
+server.on("close", async () => await disConnectDB());
