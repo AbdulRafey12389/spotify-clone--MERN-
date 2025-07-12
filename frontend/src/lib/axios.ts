358c7db98d1d0a89a6248854fd445c66aee5e747
Abdul Rafey <abdulrafey12389@gmail.com>
2025-07-12 11:15:55 -0700

    url change

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 
 
 export const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5000/api',
-})
+  baseURL: 'https://spotify-clone-mern.vercel.app/api',
+});
